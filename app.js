9669842367310b2f3d0f7b4e297624271c74fdcc
Dilyara Arslanova <risharsivil@gmail.com>
2023-04-24 15:49:52 +0300

    fix(extended-1.2): add alt attribute

--- app.js
+++ app.js
@@ -47,6 +47,7 @@ var createNewTaskElement=function(taskString){
   deleteButton.className="btn delete";
   deleteButtonImg.src='./remove.svg';
   deleteButtonImg.className="delete-icon";
+  deleteButtonImg.setAttribute("alt", "delete");
   deleteButton.appendChild(deleteButtonImg);
 
 
